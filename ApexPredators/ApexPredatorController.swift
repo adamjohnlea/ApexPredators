51282a6bed1de1a49de2e3e1a72b253a3e682812
adamjohnlea <imnogeekdptcom@me.com>
2022-12-27 11:03:27 -0700

    create and initialize predator controller

---
+++ ApexPredators/ApexPredatorController.swift
@@ -0,0 +1,29 @@
+//
+//  ApexPredatorController.swift
+//  ApexPredators
+//
+//  Created by Adam Lea on 12/27/22.
+//
+
+import Foundation
+
+class PredatorController {
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding json data: \(error)")
+            }
+        }
+    }
+}
