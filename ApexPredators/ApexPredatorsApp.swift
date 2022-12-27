b10aa98ab426b679e4d47328c2a7d8d62105a59f
adamjohnlea <imnogeekdptcom@me.com>
2022-12-27 10:27:19 -0700

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Adam Lea on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
