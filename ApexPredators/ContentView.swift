c56ae04ab7941379911b77b728bbcd80f18f4dd6
adamjohnlea <imnogeekdptcom@me.com>
2022-12-27 14:28:56 -0700

    add menu, picker, and animation (final touches)

--- ApexPredators/ContentView.swift
+++ ApexPredators/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 struct ContentView: View {
     let apController = PredatorController()
     @State var sortAlphabetical = false
+    @State var currentFilter = "All"
     
     var body: some View {
+        apController.filterBy(type: currentFilter)
+        
+        
         if sortAlphabetical {
             apController.sortByAlphabetical()
         } else {
@@ -27,10 +31,12 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Apex Predators")
-            .toolbar{
+            .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button {
-                        sortAlphabetical.toggle()
+                        withAnimation {
+                            sortAlphabetical.toggle()
+                        }
                     } label: {
                         if sortAlphabetical {
                             Image(systemName: "film")
@@ -38,6 +44,23 @@ struct ContentView: View {
                         } else {
                             Image(systemName: "textformat")
                         }
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection: $currentFilter.animation()) {
+                            ForEach(apController.typeFilters, id: \.self) { type in
+                                HStack {
+                                    Text(type)
+                                    Spacer()
+                                    Image(systemName: apController.typeIcon(for: type))
+                                }
+                            }
+                            
+                        }
+                    } label: {
+                        Image(systemName:"slider.horizontal.3")
                     }
                 }
             }
